78bfc31b1511104b012665061530012c6137b622
ingelieur <ingelieur@gmail.com>
2017-07-20 14:06:15 +0700

    Revert "populate participants in get meeting by participants"
    This reverts commit ce6c93b8974280cbf95b75be5704847806d3bc18.

--- controllers/meetupController.js
+++ controllers/meetupController.js
@@ -55,7 +55,6 @@ methods.getMeetupByParticipant = (req, res) => {
       {"participants.status": {$ne: 'no'}},
     ]}
   ]})
-    .populate('creator participants.user')
     .exec((error, response) => {
       if(error) res.json({error})
       res.json(response)
